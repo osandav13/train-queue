cbe4d14fd9823bd40f11d66766c78b2c99d6e510
Osanda <osanda.v13@gmail.com>
2020-04-09 21:31:46 +0530

    part of option "A" gui adde

--- src/Passenger.java
+++ src/Passenger.java
@@ -1,15 +1,33 @@
 public class Passenger {
     private String firstName;
-    private String surName;
+    //private String surName;
+    private String seatNumber;
+    private String nicNumber;
     private int secondsInQueue;
 
-    public void setName(String name,String surName){
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getNicNumber() {
+        return nicNumber;
+    }
+
+    public void setNicNumber(String nicNumber) {
+        this.nicNumber = nicNumber;
+    }
+
+    public void setName(String name){
         this.firstName = name;
-        this.surName = surName;
+        //this.surName = surName;
     }
 
     public String getName(){
-        return firstName + " " + surName;
+        return firstName ;
     }
 
     public void setSecondsInQueue(int seconds){
--- src/TrainStation.java
+++ src/TrainStation.java
@@ -3,9 +3,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -28,9 +26,12 @@ import java.util.Scanner;
 
 
 public class TrainStation extends Application{
-    private Passenger[] waitingRoom;
+    private final int NUM_OF_PASSENGERS = 42;
+    private Passenger[] waitingRoom = new Passenger[NUM_OF_PASSENGERS];
+    private Button[] buttonsArray = new Button[NUM_OF_PASSENGERS];
+    private int passengerCounter;
     private PassengerQueue trainQueue;
-    private List<List<String>> passengerList = new ArrayList<>();
+    private List<String[]> passengerList = new ArrayList<>();
 
     private void addingToQueue(Stage window){
 
@@ -45,9 +46,41 @@ public class TrainStation extends Application{
         datePickerLabel.setLayoutX(20);
         datePickerLabel.setLayoutY(85);
 
+        ComboBox<String> leavingStation = new ComboBox<String>();
+        leavingStation.getItems().addAll("Colombo","Badulla");
+        leavingStation.setValue("Colombo");
+        leavingStation.setLayoutX(65);
+        leavingStation.setLayoutY(230);
+        Label comboboxLable1 = new Label("From");
+        comboboxLable1.setLayoutX(20);
+        comboboxLable1.setLayoutY(235);
+        ComboBox<String> arrivingStation = new ComboBox<String>();
+        arrivingStation.getItems().addAll("Colombo","Badulla");
+        arrivingStation.setValue("Badulla");
+        arrivingStation.setLayoutX(65);
+        arrivingStation.setLayoutY(285);
+        Label comboboxLable2 = new Label("To");
+        comboboxLable2.setLayoutX(20);
+        comboboxLable2.setLayoutY(290);
 
+        ListView<String> passengerListView = new ListView<>();
+        passengerListView.setMaxHeight(150);
+        passengerListView.setLayoutX(450);
+        passengerListView.setLayoutY(80);
 
-        datePicker.setOnAction(new EventHandler<ActionEvent>() {
+        Button addToWaitingRoom = new Button("Add to waiting room");
+        addToWaitingRoom.setLayoutX(500);
+        addToWaitingRoom.setLayoutY(240);
+
+        for( int buttonNum = 0; buttonNum < NUM_OF_PASSENGERS; buttonNum++){
+            buttonsArray[buttonNum] = new Button("" + (buttonNum + 1));
+            buttonsArray[buttonNum].setVisible(false);
+            root.getChildren().add(buttonsArray[buttonNum]);
+            buttonsArray[buttonNum].setPrefWidth(45);
+        }
+
+        root.getChildren().addAll(passengerListView,addToWaitingRoom);
+        EventHandler<ActionEvent> loadPassengers = new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
@@ -58,19 +91,63 @@ public class TrainStation extends Application{
                 MongoDatabase myDB = myclient.getDatabase("cwIntegration");
                 MongoCollection<Document> myCollection = myDB.getCollection("bookingDetails");
 
+                passengerListView.getItems().clear();
+                passengerList.clear();
 
-                FindIterable<Document> findIterable = myCollection.find(eq("details",formattedDate));
+                FindIterable<Document> findIterable = myCollection.find(and(eq("date",formattedDate),eq("arrivingStation",arrivingStation.getValue())));
                 int i =0;
-                for (Document document: findIterable){
-                    List<String> list = (List<String>) document.get("details");
-                    passengerList.add(list);
-                    System.out.println(passengerList.get(i));
+                for (Document record: findIterable){
+                    String bookedDate = (String) record.get("date");
+                    String bookedSeat = (String) record.get("seat");
+                    String bookedName = (String) record.get("name");
+                    String bookedNicNum = (String) record.get("nicNumber");
+                    String bookedDepartingStation = (String) record.get("departingStation");
+                    String bookedArrivingStation = (String) record.get("arrivingStation");
+                    String[] bookingDetailsArray = {bookedDate,bookedSeat,bookedName,bookedNicNum,bookedDepartingStation,bookedArrivingStation};
+                    passengerList.add(bookingDetailsArray);
+
+                    passengerListView.getItems().add(bookedName + " " + bookedNicNum);
+                   /* System.out.println(passengerList.get(i)[0]);
+                    System.out.println(passengerList.get(i)[1]);
+                    System.out.println(passengerList.get(i)[2]);
+                    System.out.println(passengerList.get(i)[3]);
+                    System.out.println(passengerList.get(i)[4]);
+                    System.out.println(passengerList.get(i)[5]);*/
                     i++;
                 }
             }
+        };
+
+        addToWaitingRoom.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                try{
+                    datePicker.setDisable(true);
+                    arrivingStation.setDisable(true);
+                    leavingStation.setDisable(true);
+                    int recordIndex = passengerListView.getSelectionModel().getSelectedIndex();
+                    Passenger passenger = new Passenger();
+                    passenger.setSeatNumber(passengerList.get(recordIndex)[1]);
+                    passenger.setName(passengerList.get(recordIndex)[2]);
+                    passenger.setNicNumber(passengerList.get(recordIndex)[3]);
+                    passengerListView.getItems().remove(recordIndex);
+                    waitingRoom[passengerCounter] = passenger;
+                    //System.out.println(passengerList.get(recordIndex)[1] + " " + passengerList.get(recordIndex)[2]+ " " + passengerList.get(recordIndex)[3]);
+                    //System.out.println(passenger.getName() +" "+ passenger.getNicNumber()+" " + passenger.getSeatNumber());
+                    passengerList.remove(recordIndex);
+                }catch (ArrayIndexOutOfBoundsException e){
+                    System.out.println("You have not selected a passenger");
+                }
+                passengerCounter++;
+            }
         });
 
-        root.getChildren().addAll(datePicker,datePickerLabel);
+        datePicker.setOnAction(loadPassengers);
+        arrivingStation.setOnAction(loadPassengers);
+        leavingStation.setOnAction(loadPassengers);
+
+        root.getChildren().addAll(datePicker,leavingStation,arrivingStation);
+        root.getChildren().addAll(datePickerLabel,comboboxLable1,comboboxLable2);
         window.show();
 
         //selector(window);
